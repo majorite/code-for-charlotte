89a1fb85dae9dfc8bd24cb1356345f3b9f186993
Ryan <ryan.edge@zoho.com>
2018-02-02 02:06:00 -0500

    Initial commit

---
+++ website/siteConfig.js
@@ -0,0 +1,45 @@
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'User1',
+    image: '/test-site/img/docusaurus.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'CodeForCharlotte' /* title for your website */,
+  tagline: 'Become an open source contributor in no time!',
+  url: 'https://chimon20000.github.io' /* your website url */,
+  baseUrl: '/code-for-charlotte/' /* base url for your project */,
+  projectName: 'code-for-charlotte',
+  headerLinks: [
+    {doc: 'getting_started', label: 'Docs'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
+  users,
+  /* path to images for header/footer */
+  favicon: 'img/CFC-favicon.png',
+  /* colors for website */
+  colors: {
+    primaryColor: '#3d9cd1',
+    secondaryColor: '#3d9cd1',
+  },
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' Your Name or Your Company Name',
+  organizationName: 'chimon2000', // or set an env variable ORGANIZATION_NAME
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+  scripts: ['https://buttons.github.io/buttons.js'],
+  // You may provide arbitrary config keys to be used as needed by your template.
+  repoUrl: 'https://github.com/chimon2000/code-for-charlotte',
+};
+
+module.exports = siteConfig;
